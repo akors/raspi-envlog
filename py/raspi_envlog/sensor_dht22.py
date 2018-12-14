375dbd79a9f4386f035b6e1024f34378248b296e
Alexander Korsunsky <a.korsunsky@gmail.com>
2018-12-14 16:36:52 +0100

    sensor_dht22: Try to catch invalid sensor readings

--- py/raspi_envlog/sensor_dht22.py
+++ py/raspi_envlog/sensor_dht22.py
@@ -1,6 +1,7 @@
 
 from typing import Tuple
 
+import math
 import datetime
 import configparser
 import json
@@ -10,10 +11,6 @@ import Adafruit_DHT
 # https://github.com/nicmcd/vcgencmd
 import vcgencmd
 
-THISCONF = 'sensor_dht22'
-
-
-
 class Sensor:
     def __init__(self, config: configparser.ConfigParser):
         self.__devices = json.loads(config["sensor_dht22"].get("devices"))
@@ -22,8 +19,17 @@ class Sensor:
         measurements = list()
 
         for sn, pin in self.__devices.items():
-            (humidity, temperature) = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, pin)
+            (humidity, temperature) = Adafruit_DHT.read_retry(
+                Adafruit_DHT.AM2302,
+                pin,
+                retries=5,
+                delay_seconds=2.5)
             timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
+
+            # Sometimes we get crap values from the sensor. Try to detect these
+            # and skip the readings
+            if math.isnan(humidity) or math.isnan(temperature) or humidity > 100.0:
+                continue
 
             measurements.append({
                 "measurement": "temperature",
